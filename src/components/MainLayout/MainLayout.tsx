0a8741c91d2a216ff47f3954fa68248ddc1274d6
Morgan Cooper <morgan_cooper@epam.com>
2023-02-21 16:45:40 +0000

    adds deploy script and makes small change to app to view deployment from cli

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -25,6 +25,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <main>
         <Container sx={{ pb: 8 }} maxWidth="md">
           {children}
+          <h1>Hello lads, I have been deployed to cloudfront using SDK</h1>
         </Container>
       </main>
       <Box
